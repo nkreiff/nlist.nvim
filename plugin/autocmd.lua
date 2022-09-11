ad4a1be22d31782b4ab040d07a5f763264167d03
Nicolás Kreiff <nicolas.kreiff@mercadolibre.com>
2022-09-11 12:27:18 -0300

    FIX: local variable name

--- plugin/autocmd.lua
+++ plugin/autocmd.lua
@@ -1,9 +1,9 @@
 local mappings = require("nlist.mappings")
 
-local ngit_augroup = vim.api.nvim_create_augroup("nList", { clear = true })
+local nlist_augroup = vim.api.nvim_create_augroup("nList", { clear = true })
 
 vim.api.nvim_create_autocmd("BufEnter", {
-    group = ngit_augroup,
+    group = nlist_augroup,
     pattern = "nList",
     callback = function()
         local buf = vim.api.nvim_get_current_buf()
